17d3eb557802d034b5fe68888e69961a51493330
bohan <bohan@myzoomride.com>
2020-08-03 16:50:03 -0400

    done:SmallInfo BrefSchedule Basic Info

---
+++ src/view/ItemForm.vue
@@ -0,0 +1,194 @@
+<template>
+    <div>
+        <div class="crumbs">
+            <el-breadcrumb separator="/">
+                <el-breadcrumb-item><i class="el-icon-lx-calendar"></i> 表单</el-breadcrumb-item>
+                <el-breadcrumb-item>Add item page</el-breadcrumb-item>
+            </el-breadcrumb>
+        </div>
+        <div class="container">
+            <div class="form-box">
+                <el-form ref="form" :model="form" label-width="80px">
+                    <h4>Basic Information</h4>
+                    <el-form-item label="Operator">
+                        <input type="text" disabled="" :value="form.userId" v-show="false">{{username}}
+                    </el-form-item>
+                    <el-form-item label="type">
+                        <el-select v-model="form.type" placeholder="Type">
+                            <el-option key="1" label="Tour" value="1"></el-option>
+                            <el-option key="2" label="Hotel" value="2"></el-option>
+                            <el-option key="3" label="Restaurant" value="3"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Kind">
+                        <el-select v-model="form.kind" placeholder="Kind">
+                            <el-option key="1" label="Kind1" value="1"></el-option>
+                            <el-option key="2" label="Kind2" value="2"></el-option>
+                            <el-option key="3" label="Kind3" value="3"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit" v-show="currentStep === 1">Create</el-button>
+                        <el-button>Cancel</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+        </div>
+        <div class="container" v-show="currentStep >= 2">
+            <div class="form-box">
+                <el-form ref="form" :model="smallInfo" label-width="80px">
+                    <h4>Small Information</h4>
+                    <el-form-item label="Item Type">
+                        <el-select v-model="smallInfo.type" placeholder="Type">
+                            <el-option key="1" label="popular" value="popular"></el-option>
+                            <el-option key="2" label="topRate" value="topRate"></el-option>
+                            <el-option key="3" label="standard" value="standard"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Image Url">
+                        <el-input v-model="smallInfo.imgUrl"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Title">
+                        <el-input v-model="smallInfo.title"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Description">
+                        <el-input v-model="smallInfo.des"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Past Price" size="small">
+                        <el-input v-model="smallInfo.pPrice"></el-input>
+                    </el-form-item>
+                    <el-form-item label="Current Price" size="mini">
+                        <el-input v-model="smallInfo.cPrice"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit2" v-show="currentStep === 2">Save This Part
+                        </el-button>
+                        <el-button>Cancel</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+            <div></div>
+        </div>
+
+        <div class="container" v-show="currentStep >= 3">
+            <div class="form-box">
+                <el-form ref="form" :model="brefSchedule" label-width="80px">
+                    <h4>Brief schedule & address</h4>
+                    <el-form-item label="Address">
+                        <el-input v-model="brefSchedule.address"></el-input>
+                    </el-form-item>
+                    <h4>Schedule</h4>
+                    <el-form-item label="Moday">
+                        <el-select v-model="brefSchedule.mo" placeholder="duration">
+                            <el-option v-for="item in scheduleItem" :key="item.id" :label="item.content" :value="item.content"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Tuesday">
+                        <el-select v-model="brefSchedule.tu" placeholder="duration">
+                            <el-option v-for="item in scheduleItem" :key="item.id" :label="item.content" :value="item.content"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Wednesday">
+                        <el-select v-model="brefSchedule.we" placeholder="duration">
+                            <el-option v-for="item in scheduleItem" :key="item.id" :label="item.content" :value="item.content"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Thursday">
+                        <el-select v-model="brefSchedule.th" placeholder="duration">
+                            <el-option v-for="item in scheduleItem" :key="item.id" :label="item.content" :value="item.content"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Friday">
+                        <el-select v-model="brefSchedule.fr" placeholder="duration">
+                            <el-option v-for="item in scheduleItem" :key="item.id" :label="item.content" :value="item.content"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Saturday">
+                        <el-select v-model="brefSchedule.sa" placeholder="duration">
+                            <el-option v-for="item in scheduleItem" :key="item.id" :label="item.content" :value="item.content"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="Sunday">
+                        <el-select v-model="brefSchedule.su" placeholder="duration">
+                            <el-option v-for="item in scheduleItem" :key="item.id" :label="item.content" :value="item.content"></el-option>
+                        </el-select>
+                    </el-form-item>
+
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit2" v-show="currentStep === 3">Save This Part
+                        </el-button>
+                        <el-button>Cancel</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+            <div></div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'itemForm',
+        data: function () {
+            return {
+                scheduleItem:[
+                    {content:'closed',id:'1'},
+                    {content:'9:00AM - 10:00PM',id:'2'},
+                    {content:'9:00AM - 5:00PM',id:'3'}
+                ],
+                username: 'Bohan',
+                currentStep: 1,
+                form: {
+                    userId: '1',
+                    type: '',
+                    kind: ''
+                },
+                smallInfo: {
+                    userId: '1',
+                    itemId: '1111',
+                    type: '',
+                    imgUrl: '',
+                    title: '',
+                    des: '',
+                    cPrice: '',
+                    pPrice: ''
+                },
+                brefSchedule: {
+                    userId: '1',
+                    itemId: '1111',
+                    address: '',
+                    mo: '',
+                    tu: '',
+                    we: '',
+                    th: '',
+                    fr: '',
+                    sa: '',
+                    su: ''
+                },
+            }
+        },
+        methods: {
+            onSubmit() {
+                console.log(JSON.stringify(this.form));
+                this.$message.success('Create success！');
+                this.currentStep++;
+            },
+            onSubmit2() {
+                console.log(JSON.stringify(this.form));
+                this.$message.success('Save success！');
+                this.currentStep++;
+            }
+        },
+        created() {
+
+        }
+    }
+</script>
+
+<style scoped>
+    h4 {
+        margin-bottom: 10px;
+    }
+
+</style>
